830badbd731bf0136894137307630f2101f3952e
Alisson Matias <alissonmds00@gmail.com>
2022-07-15 19:21:49 -0300

    feat: adicionada a base javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+let segs = document.getElementById('segs')
+let mins = document.getElementById('mins')
+let hora = document.getElementById('hora')
+let dias = document.getElementById('dia')
+
+
+while (hora > 0 && mins > 0) {
+
+}
