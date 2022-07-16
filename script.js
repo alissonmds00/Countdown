336743693eafc89636e84a4ceca011324a878751
Alisson Matias <alissonmds00@gmail.com>
2022-07-16 02:00:39 -0300

    feat: adicionadas explicações

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const mins = document.querySelector('#mins');
 const hours = document.querySelector('#hora');
 const days = document.querySelector('#dia');
 
-const year = '26 Jul 2022';
+const data = '26 Jul 2022';
 function countdown() {
-    const newYearsDate = new Date(year)
+    const newYearsDate = new Date(data)
     const CurrentDate = new Date()
     const totalSeconds = (newYearsDate - CurrentDate) / 1000
     const Days = Math.floor(totalSeconds / 3600 / 24)
@@ -22,8 +22,10 @@ function countdown() {
 
 function formatTime(time) {
     return time < 10 ? `0${time}` : time
+    /* se o valor for menor que 10, então adiciona o valor 0 antes do número */
 }
 
 countdown()
 setInterval(countdown, 1000)
+/* define o intervalo de 1 segundo*/
 
