b747ec49bb48b7090ac1aa46ee8786870a21070e
Alisson Matias <alissonmds00@gmail.com>
2022-07-16 23:44:10 -0300

    feat: adicionados comentários explicativos

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ const data = '26 Jul 2022';
 function countdown() {
     const newYearsDate = new Date(data)
     const CurrentDate = new Date()
-    const totalSeconds = (newYearsDate - CurrentDate) / 1000
-    const Days = Math.floor(totalSeconds / 3600 / 24)
-    const Hours = Math.floor(totalSeconds / 3600) % 24
-    const Mins = Math.floor(totalSeconds / 60) % 60
-    const Secs =  Math.floor(totalSeconds) % 60
+    const totalSeconds = (newYearsDate - CurrentDate) / 1000 // (data atual / 1000) para transformar o tempo para 1 segundo corrido 
+    const Days = Math.floor(totalSeconds / 3600 / 24) // segundos totais divididos por 3600 (hora) dividido por 24 (transforma para dias)
+    const Hours = Math.floor(totalSeconds / 3600) % 24 // O restante dos segundos que não foram convertidos para dias, irão ser transformados em horas
+    const Mins = Math.floor(totalSeconds / 60) % 60 // O restante dos segundos será transformado em segundo, o valor que restar
+    const Secs =  Math.floor(totalSeconds) % 60 // Todo restante será atribuído aos segundos
 
     days.innerHTML = formatTime(Days)
     hours.innerHTML = formatTime(Hours)
